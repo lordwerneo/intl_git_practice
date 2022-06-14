1be904c652ade0dd0c0c00738bce506036c4c2b2
jlTsZGpw <lordwerneo@gmail.com>
2022-06-14 14:15:30 +0300

    Fixed magic numbers. lol

--- datatypes/main.go
+++ datatypes/main.go
@@ -12,8 +12,12 @@ const (
 )
 
 func main() {
-	fmt.Printf("We need %v of money to buy 9 apples and 8 pears.\n", (applePrice*9)+(pearPrice)*8)
+	applesToBuy := 9
+	pearsToBuy := 8
+	fmt.Printf("We need %v of money to buy 9 apples and 8 pears.\n", (applePrice*float64(applesToBuy))+(pearPrice)*float64(pearsToBuy))
 	fmt.Printf("We can buy %v pears.\n", math.Floor(purse/pearPrice))
 	fmt.Printf("We can buy %v apples.\n", math.Floor(purse/applePrice))
-	fmt.Printf("Can we buy 2 apples and 2 pears? - %v \n", (applePrice*2)+(pearPrice*2) <= purse)
+	applesToBuy = 2
+	pearsToBuy = 2
+	fmt.Printf("Can we buy 2 apples and 2 pears? - %v \n", (applePrice*float64(applesToBuy))+(pearPrice*float64(pearsToBuy)) <= purse)
 }
